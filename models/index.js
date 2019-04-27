a7623e5e017b7760e8ac42ef53709d04b3f70b52
Eric Dahan <Ericdahan@dyn-160-39-244-18.dyn.columbia.edu>
2019-04-27 14:05:50 -0400

    Heroku deployment 6

--- models/index.js
+++ models/index.js
@@ -4,21 +4,13 @@ const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || "development";
-const config = require("../config/config.json")[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
+const sequelize = new Sequelize(
+  "ilgd6w3fv2b1hprm",
+  "xfwtxxtle8ag4noj",
+  "wiycii6vuedplk2b"
+);
 
 fs.readdirSync(__dirname)
   .filter(file => {
