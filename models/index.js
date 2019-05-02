9594fa986abb7bafb5b6dbcfd4c5f32b94dba70d
hchen651 <45304484+hchen651@users.noreply.github.com>
2019-05-02 19:13:42 -0400

    Update index.js

--- models/index.js
+++ models/index.js
@@ -21,11 +21,11 @@ const db = {};
 //);
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
+//if (config.use_env_variable) {
+//  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+//} else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+//}
 
 fs.readdirSync(__dirname)
   .filter(file => {
