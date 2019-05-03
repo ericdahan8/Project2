4ea2e30d95313ad9b2d1da8d8ba20f148ecf0868
J Lobos Vega <jalv.vg@gmail.com>
2019-05-03 19:57:59 -0400

    addedmap to results page

--- public/js/camera.js
+++ public/js/camera.js
@@ -8,14 +8,5 @@ const app = new Clarifai.App({
  apiKey: '0e3c7332ea1c461fb29b8804a6aa0eef'
 });
 
-app.models.initModel({id: "3e5c3c5da6204f869d6efe680d170133", version: "0e3c7332ea1c461fb29b8804a6aa0eef"})
-.then(generalModel => {
-  return generalModel.predict("https://samples.clarifai.com/metro-north.jpg");
-})
-.then(response => {
-  var concepts = response['outputs'][0]['data']['concepts']
-  console.log(concepts);
-})
-.catch(error => {
-    console.log(error.data);
+process.nextTick = setImmediate;
 });
